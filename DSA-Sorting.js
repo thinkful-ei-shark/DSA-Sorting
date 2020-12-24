9926b123837c132b2ff4dc178f1a96d07fe57fe1
Evisari Riyanto <evisari111012@gmail.com>
2020-12-24 15:07:01 -0800

    4 drills

--- DSA-Sorting.js
+++ DSA-Sorting.js
@@ -7,4 +7,63 @@
 //  right array: [16,27,34,39,40,43,46,49]
 //d. [28, 29] [2,9]
 
-//2.
+//2a. The pivot could have been either 14 or 17.
+//2b. 1. [13, 10, 3, 9, 12,14,17,15,19,16]
+//2. [10,3,9,12,14,17,13,15,19,16]
+
+//    3. Implementing quicksort
+function quicksort(array, leftBound = 0, rightBound = array.length - 1) {
+  if (leftBound < rightBound) {
+    const pivotIndex = partition(array, leftBound, rightBound);
+    quicksort(array, leftBound, pivotIndex - 1);
+    quicksort(array, pivotIndex, rightBound);
+  }
+  return array;
+}
+
+function partition(array, leftIndex, rightIndex) {
+  const pivot = array[Math.floor(leftIndex + rightIndex) / 2];
+  while (leftIndex <= rightIndex) {
+    while (array[leftIndex] < pivot) {
+      leftIndex++;
+    }
+    while (array[rightIndex] > pivot) {
+      rightIndex--;
+    }
+    if (leftIndex <= rightIndex) {
+      swap(array, leftIndex, rightIndex);
+      leftIndex++;
+      rightIndex--;
+    }
+  }
+  return leftIndex;
+}
+
+// 4. Implementing merge sort
+const mergeSort = (startArray) => {
+  const length = startArray.length;
+  if (length === 1) {
+    return startArray;
+  }
+
+  const mid = Math.floor(length / 2);
+  const leftArray = startArray.slice(0, mid);
+  const rightArray = startArray.slice(mid, length);
+
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
+};
+
+const merge = (leftArray, rightArray) => {
+  const sortedArray = [];
+  while (leftArray.length > 0 && rightArray.length > 0) {
+    if (leftArray[0] < rightArray[0]) {
+      sortedArray.push(leftArray[0]);
+      leftArray.shift();
+    } else {
+      sortedArray.push(rightArray[0]);
+      rightArray.shift();
+    }
+  }
+
+  return sortedArray.concat(leftArray).concat(rightArray);
+};
